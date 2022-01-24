bc9a2f0a6bd48a58dbe397e240708caf2962f58c
mironenko_ev@mail.ru <mironenko_ev@mail.ru>
2022-01-24 18:05:38 +0500

    air+water

--- Abstract_Class/main.cpp
+++ Abstract_Class/main.cpp
@@ -7,9 +7,48 @@ class Vehicle
 public:
 	virtual void move() = 0;//чисто виртуальный метод именно этот метод делает класс абстрактным
 };
-class GroundVehicle:public Vehicle
+class WaterVehicle :public Vehicle
 {
 
+};
+class Yacht : public WaterVehicle
+{
+public:
+	void move()
+	{
+		cout << "Яхта плывет под парусом" << endl;
+	}
+};
+class Boat : public WaterVehicle
+{
+public:
+	void move()
+	{
+		cout << "Катер плывет при помощи мотора" << endl;
+	}
+};
+class AirVehicle:public Vehicle
+{
+
+};
+class Airplane : public AirVehicle
+{
+public:
+	void move()
+	{
+		cout << "Самолет летит с помошью тяги от двигателя" << endl;
+	}
+};
+class Helicopter :public AirVehicle
+{
+public:
+	void move()
+	{
+		cout << "Вертолет летит за счет движущихся лопастей" << endl;
+	}
+};
+class GroundVehicle:public Vehicle
+{
 
 };
 
@@ -35,4 +74,16 @@ void main()
 	//Vehicle v;
 	Car bmw;
 	bmw.move();
+
+	Yacht PeriniNavi;
+	PeriniNavi.move();
+
+	Boat SeaRay;
+	SeaRay.move();
+
+	Airplane Boeing;
+	Boeing.move();
+
+	Helicopter Robinson;
+	Robinson.move();
 }
